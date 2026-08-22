6a6c68ddb7d789c210550becfea3258828e11e51
swmyhome <139029887+swmyhome@users.noreply.github.com>
2026-08-22 20:48:29 +0200

    Create input group and update hermes user
    Add input group and modify hermes user permissions

--- hermes-agent/run.sh
+++ hermes-agent/run.sh
@@ -40,6 +40,9 @@ chown -R hermes:hermes /share/hermes 2>/dev/null || true
 echo "hermes ALL=(ALL) NOPASSWD: /usr/bin/apt-get" > /etc/sudoers.d/hermes                                                     
 chmod 0440 /etc/sudoers.d/hermes  
 
+groupadd -g 110 input 2>/dev/null || true
+usermod -a -G audio,input hermes
+
 #
 # Create persistent directories
 #
